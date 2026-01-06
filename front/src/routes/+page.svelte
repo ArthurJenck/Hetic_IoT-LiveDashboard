d014f4ab8c565b7cb7f4cbdbac44abfbd5f39b63
Arthur Jenck <arthurjenckdev@gmail.com>
2026-01-06 12:17:56 +0100

    feat: svelte front setup

---
+++ front/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  let count = 0;
+
+  const increment = () => {
+    count++;
+  };
+</script>
+
+<div class="size-screen flex h-screen flex-col items-center justify-center gap-2">
+  <p class="text-xl">Compteur {count} !</p>
+  <button onclick={increment} class="rounded-lg border px-2 py-1">Cliquer</button>
+</div>
