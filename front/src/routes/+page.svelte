cca67490d5c4760345658f2f84a1f2a33e153f23
Arthur Jenck <arthurjenckdev@gmail.com>
2026-01-06 12:23:05 +0100

    initial style

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -7,6 +7,6 @@
 </script>
 
 <div class="size-screen flex h-screen flex-col items-center justify-center gap-2">
-  <p class="text-xl">Compteur {count} !</p>
-  <button onclick={increment} class="rounded-lg border px-2 py-1">Cliquer</button>
+  <p class="text-xl font-semibold">Compteur {count} !</p>
+  <button onclick={increment} class="rounded-lg border px-2 py-1 text-white bg-neutral-800 cursor-pointer hover:bg-neutral-700">Cliquer ici</button>
 </div>
